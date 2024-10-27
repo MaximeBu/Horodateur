366a7214113e45e7459839155a8d51d7775929b8
Maxime Bureau <142453794+MaximeBu@users.noreply.github.com>
2024-10-27 01:06:25 +0100

    Add files via upload

---
+++ arduino/arduino.ino
@@ -0,0 +1,18 @@
+#define yAxisPin A0
+
+int yValue;
+
+void setup() {
+  Serial.begin(9600); // Initialisation du Serial
+  Serial.flush();
+  pinMode(yAxisPin, INPUT);
+}
+
+void loop() {
+  yValue = analogRead(yAxisPin); // Lecture des données du Joystick
+  yValue = map(yValue, 0, 1023, 0, 180); // Formatage des données vers un angle entre 0 et 180
+
+  Serial.println(yValue);
+
+  delay(200);
+}
