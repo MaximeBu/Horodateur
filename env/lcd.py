db5f36d5d089b1e2c169b7bf8dc32b602fa524ff
Maxime Bureau <142453794+MaximeBu@users.noreply.github.com>
2024-10-28 15:54:52 -0400

    Update lcd.py

--- env/lcd.py
+++ env/lcd.py
@@ -11,8 +11,14 @@ lcd.clear()
 # Fonction pour récupérer l'heure actuelle
 def getTime():
   now = datetime.now(timezone("Canada/Eastern"))
-  current_time = now.strftime("  %Hh:%M")
+  current_time = now.strftime("%Hh:%M")
   return current_time
+
+# Fonction pour récupérer la date actuelle
+def getDate():
+  now = datetime.now()
+  current_date = now.strftime("%d/%m/%y")
+  return current_date
 
 # Fonction d'affichage du input pour le code
 def show_code_screen(code):
@@ -23,8 +29,9 @@ def show_code_screen(code):
 # Fonction d'affichage du menu 1
 def show_menu_start():
   lcd.clear()
-  lcd.write_string("Menu:   ")
   lcd.write_string(getTime())
+  lcd.write_string("  ")
+  lcd.write_string(getDate())
   lcd.crlf()
   lcd.write_string("Choisir de 1 a 4")
 
@@ -67,17 +74,22 @@ def show_message(option, nom):
   match option:
     case 1:
       lcd.write_string("Bonne journee!")
-      print(nom, " commence sa journee")
+      print(nom, " commence sa journée")
     case 2:
       lcd.write_string("Au revoir!")
-      print(nom, " a finit sa journee")
+      print(nom, " a finit sa journée")
     case 3:
       lcd.write_string("Bon repos!")
       print(nom, " prend sa pause")
     case 4:
       lcd.write_string("On y retourne!")
-      print(nom, " a finti sa pause")
+      print(nom, " a finit sa pause")
     case _:
       print("Option inexistante")
   sleep(2)
+
+# Fonction d'affichage de l'erreur
+def show_error():
+  lcd.clear()
+  lcd.write_string("Erreur!")
     
