7b07a29d61ebd359d69afd07038bb07e2e8e1cb3
ThiThaoChi <e2162278@cmaisonneuve.qc.ca>
2024-11-29 16:17:07 -0500

    itération 3

---
+++ env/horodateur-punch/Codes/user.py
@@ -0,0 +1,30 @@
+# Utilisateurs du système
+users = {
+  "Maxime Bureau" : "1111",
+  "Martin Ndjiya" : "2038",
+  "Thao Chi Tran" : "9210",
+  "Aysha Hossain" : "4290",
+}
+
+nom = ""
+
+# Fonction qui récupère le nom de l'utilisateur qui se connecte
+def getNom():
+  return nom
+
+# Fonction qui attribut le nom de l'utilisateur qui se connecte
+def setNom(newName):
+  global nom
+  nom = newName
+
+# Fonction de validation du code saisi
+def validate_user(code):
+  for nom, id in users.items():
+    if id == code:
+      setNom(nom)
+      break
+      
+def resetNom():
+  global nom
+  nom = ""
+
